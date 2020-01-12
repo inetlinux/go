ab426377b3abe39852f3b7c8900424b604e403f4
Jing Li <lijing@example.com>
2020-01-12 20:03:07 +0800

    add docker, ini, redis dependencies

--- main.go
+++ main.go
@@ -9,8 +9,15 @@ import (
 
 	_ "go.etcd.io/etcd/clientv3"
 
+	_ "github.com/bitly/go-simplejson"
 	_ "github.com/dgrijalva/jwt-go"
+	_ "github.com/docker/docker/api/types"
+	_ "github.com/docker/docker/api/types/container"
+	_ "github.com/docker/docker/api/types/mount"
+	_ "github.com/docker/docker/client"
 	_ "github.com/gin-gonic/gin"
+	_ "github.com/go-ini/ini"
+	_ "github.com/go-redis/redis"
 	_ "github.com/golang/protobuf"
 	_ "github.com/gorilla/context"
 	_ "github.com/gorilla/mux"
@@ -19,7 +26,10 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/patrickmn/go-cache"
 	_ "github.com/urfave/cli"
+	_ "github.com/wxnacy/wgo/arrays"
 
+	_ "golang.org/x/crypto/ssh"
+	_ "golang.org/x/crypto/ssh/terminal"
 	_ "golang.org/x/net"
 	_ "golang.org/x/sys"
 	_ "google.golang.org/genproto"
